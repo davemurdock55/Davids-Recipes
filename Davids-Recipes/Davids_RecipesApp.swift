40e0406361ee00eb697e5fec9e9fd1d8d2585f33
davemurdock55 <davemurdock55@gmail.com>
2024-11-21 15:58:15 -0700

    Initial Commit

---
+++ Davids-Recipes/Davids_RecipesApp.swift
@@ -0,0 +1,32 @@
+//
+//  Davids_RecipesApp.swift
+//  Davids-Recipes
+//
+//  Created by David Murdock on 11/21/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Davids_RecipesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
