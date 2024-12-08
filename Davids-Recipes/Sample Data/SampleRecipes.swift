2253dc1b78acea8de4404032cff13ff9ed921dbb
davemurdock55 <davemurdock55@gmail.com>
2024-12-07 19:12:26 -0700

    - Added App Icon - Removed "Item" - Added ability to add images from URL to Recipes - Began cleaning up code (removing/adding comments, adding constants, etc.) - Changed "favorited" to "isFavorite" - Added the basis to filter things Prof. Liddle's way

---
+++ Davids-Recipes/Sample Data/SampleRecipes.swift
@@ -0,0 +1,88 @@
+//
+//  SeedData.swift
+//  Davids-Recipes
+//
+//  Created by David Murdock on 12/3/24.
+//
+
+import Foundation
+
+let sampleRecipes = [
+    Recipe(
+        title: "Sweet Crepes",
+        author: "David Murdock",
+        recipeQuote: "I love crepes",
+        categories: ["Breakfast", "Dessert"],
+        ingredients: """
+        |   Quantity    |   Description |   Notes   |
+        |-----|-----|-----|
+        |   2  |    eggs    |   Eggs taste good |
+        |   1/2 cup |   Milk    |   You need milk   |
+        """,
+        instructions: """
+        1. Mix it together
+        2. Cook it on a pan
+        3. Eat them and enjoy
+        """,
+        notes: "This recipe is for sweet crepes and not savory ones",
+        favorited: true,
+        lastModified: Date()
+    ),
+//    Recipe(
+//        title: <#T##String#>,
+//        author: <#T##String#>,
+//        recipeQuote: <#T##String#>,
+//        categories: <#T##[String]#>,
+//        ingredients: <#T##String#>,
+//        instructions: <#T##String#>,
+//        notes: <#T##String#>,
+//        favorited: <#T##Bool#>,
+//        lastModified: <#T##Date#>
+//    ),
+//    Recipe(
+//        title: <#T##String#>,
+//        author: <#T##String#>,
+//        recipeQuote: <#T##String#>,
+//        categories: <#T##[String]#>,
+//        ingredients: <#T##String#>,
+//        instructions: <#T##String#>,
+//        notes: <#T##String#>,
+//        favorited: <#T##Bool#>,
+//        lastModified: <#T##Date#>
+//    ),
+//    Recipe(
+//        title: <#T##String#>,
+//        author: <#T##String#>,
+//        recipeQuote: <#T##String#>,
+//        categories: <#T##[String]#>,
+//        ingredients: <#T##String#>,
+//        instructions: <#T##String#>,
+//        notes: <#T##String#>,
+//        favorited: <#T##Bool#>,
+//        lastModified: <#T##Date#>
+//    ),
+//    Recipe(
+//        title: <#T##String#>,
+//        author: <#T##String#>,
+//        recipeQuote: <#T##String#>,
+//        categories: <#T##[String]#>,
+//        ingredients: <#T##String#>,
+//        instructions: <#T##String#>,
+//        notes: <#T##String#>,
+//        favorited: <#T##Bool#>,
+//        lastModified: <#T##Date#>
+//    ),
+//    Recipe(
+//        title: <#T##String#>,
+//        author: <#T##String#>,
+//        recipeQuote: <#T##String#>,
+//        categories: <#T##[String]#>,
+//        ingredients: <#T##String#>,
+//        instructions: <#T##String#>,
+//        notes: <#T##String#>,
+//        favorited: <#T##Bool#>,
+//        lastModified: <#T##Date#>
+//    )
+    
+    
+]
